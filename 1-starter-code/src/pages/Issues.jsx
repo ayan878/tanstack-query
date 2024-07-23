d0e758489a6ac8a1b8a313532b021f5204e5f71e
ayan878 <ayanraza197@gmail.com>
2024-07-23 18:27:24 +0530

    components

---
+++ 1-starter-code/src/pages/Issues.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import IssuesList from "../components/IssuesList";
+import LabelList from "../components/LabelList";
+
+export default function Issues() {
+  const [labels, setLabels] = useState([]);
+
+  // const toggle = (label) => {
+  //   setLabels((currentLabels) =>
+  //     currentLabels.includes(label)
+  //       ? currentLabels.filter((currentLabel) => currentLabel !== label)
+  //       : currentLabels.concat(label)
+  //   );
+  // };
+
+  return (
+    <div>
+      <main>
+        <section>
+          <h1>Issues</h1>
+          <IssuesList labels={labels} />
+        </section>
+        <aside>
+          <LabelList
+            selected={labels}
+            toggle={
+              (label) => {
+                setLabels((currentLabels) =>
+                  currentLabels.includes(label)
+                    ? currentLabels.filter(
+                        (currentLabel) => currentLabel !== label
+                      )
+                    : currentLabels.concat(label)
+                );
+              }
+            }
+          />
+        </aside>
+      </main>
+    </div>
+  );
+}

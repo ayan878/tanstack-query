e1cdc724c00dfa2a3920c1c479b1daadf7dade93
ayan878 <ayanraza197@gmail.com>
2024-07-24 23:24:38 +0530

    react-query

--- 1-starter-code/src/pages/Issues.jsx
+++ 1-starter-code/src/pages/Issues.jsx
@@ -5,13 +5,7 @@ import LabelList from "../components/LabelList";
 export default function Issues() {
   const [labels, setLabels] = useState([]);
 
-  // const toggle = (label) => {
-  //   setLabels((currentLabels) =>
-  //     currentLabels.includes(label)
-  //       ? currentLabels.filter((currentLabel) => currentLabel !== label)
-  //       : currentLabels.concat(label)
-  //   );
-  // };
+  const [status, setStatus] = useState("");
 
   return (
     <div>
@@ -23,20 +17,42 @@ export default function Issues() {
         <aside>
           <LabelList
             selected={labels}
-            toggle={
-              (label) => {
-                setLabels((currentLabels) =>
-                  currentLabels.includes(label)
-                    ? currentLabels.filter(
-                        (currentLabel) => currentLabel !== label
-                      )
-                    : currentLabels.concat(label)
-                );
-              }
-            }
+            toggle={(label) => {
+              setLabels((currentLabels) =>
+                currentLabels.includes(label)
+                  ? currentLabels.filter(
+                      (currentLabel) => currentLabel !== label
+                    )
+                  : currentLabels.concat(label)
+              );
+            }}
+          />
+          <h3>Status</h3>
+          <StatusSelect
+            value={status}
+            onChange={(event) => setStatus(event.target.value)}
           />
         </aside>
       </main>
     </div>
+  );
+}
+
+const possibleStatus = [
+  { id: "backlog", label: "Backlog" },
+  { id: "todo", label: "Todo" },
+  { id: "inProgress", label: "In Progress" },
+  { id: "done", label: "Done" },
+  { id: "cancelled", label: "Cancelled" },
+];
+function StatusSelect(value, onChange) {
+  return (
+    <select value={value} onChange={onChange} className="status-select">
+      {possibleStatus.map((status) => (
+        <option key={status.id} value={status.id}>
+          {status.label}
+        </option>
+      ))}
+    </select>
   );
 }
